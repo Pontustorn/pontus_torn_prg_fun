cb1f091fc8ab81055283f0c814fee8c25f4c0634
Pontustorn <35564939+Pontustorn@users.noreply.github.com>
2021-09-15 14:00:14 +0200

    Assignment 2

---
+++ Assets/Assignment2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment2 : ProcessingLite.GP21
+{
+
+    float spaceBetweenLines = 0.2f;
+    float x2 = 1;
+    float y1 = 30;
+    float ChangeValue = 3;
+    
+
+    void Start()
+    {
+        Background(Color.white);
+        StrokeWeight(0.5f);
+
+    }
+
+    void Update()
+    {
+
+
+        for (int i = 0; i <= y1; i++)
+        {
+
+            float y = i * spaceBetweenLines;
+            x2++;
+            y1--;
+
+            if (x2 % ChangeValue == 1)
+            {
+                Stroke(102, 255, 102);
+            }
+            else
+            {
+                Stroke(0, 0, 0);
+            }
+            //Draw a line from left side of screen to the right
+            //this time modify the height depending on time passed
+
+            Line(0, y1, x2, 0);
+        }
+
+
+
+    }
+}
